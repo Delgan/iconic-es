c75f1823f26cb7c11a5f6a43f283e9c1e741a4d0
Delgan <delgan.py@gmail.com>
2026-06-28 15:20:48 +0200

    Bump Ubuntu version in devcontainer
    So that we can use a more recent version of `imagemagick`.

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -3,11 +3,13 @@ set -e
 
 mkdir -p ~/.emulationstation/themes
 
-echo "Installing Python dependencies..."
-python3 -m pip install --no-warn-script-location opencv-python requests rich pillow lxml imagehash piexeif filetype
+echo "Installing Python dependencies in virtual env..."
+python3 -m venv ~/.venv
+source ~/.venv/bin/activate
+python3 -m pip install opencv-python requests rich pillow lxml imagehash piexif filetype
 
 echo "Setting up EmulationStation configuration..."
-python3 "$WORKSPACE_DIR/_tools/setup_es_config.py" /home/dev/.emulationstation/es_systems.cfg  /home/dev/.emulationstation/gamelists /opt/roms
+python3 "$WORKSPACE_DIR/_tools/setup_es_config.py" /home/ubuntu/.emulationstation/es_systems.cfg  /home/ubuntu/.emulationstation/gamelists /opt/roms
 
 cp -r "$WORKSPACE_DIR/.devcontainer/es_input.cfg" ~/.emulationstation/
 
